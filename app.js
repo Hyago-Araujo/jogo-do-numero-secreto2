c226887ceae560b6059defb3a9d8d148c8744802
Hyago Araujo <hyago.araujo@escola.pr.gov.br>
2025-10-14 10:55:24 -0300

    ok

--- app.js
+++ app.js
@@ -7,3 +7,8 @@ function exibirTextoNaTela(tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
+
+function exibirMensagemInical(){
+    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10!');
+}
