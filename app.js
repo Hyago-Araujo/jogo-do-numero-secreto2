210e92818a8a24679bf09c535ddef8161cdd2b15
Hyago Araujo <hyago.araujo@escola.pr.gov.br>
2025-10-14 10:52:28 -0300

    ok

--- app.js
+++ app.js
@@ -3,4 +3,7 @@ let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio(); 
 let tentativas = 1;
 
-function exibirTextoNaTela
+function exibirTextoNaTela(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
